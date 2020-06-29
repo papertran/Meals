99824cdddb12ee61f940adf86848d2de443a46f9
Kevin <kevintrn99@gmail.com>
2020-06-28 22:25:11 -0400

    able to render out list

--- src/componenets/MealList.js
+++ src/componenets/MealList.js
@@ -1,3 +1,4 @@
+import '../assets/main.css'
 import React, { Component } from 'react'
 import { connect } from "react-redux";
 import { fetchMeals } from "../actions"
@@ -9,11 +10,26 @@ class MealList extends Component {
 
 	}
 
+	// Helper Function to render out meals
+	renderMealsList() {
+		return this.props.meals.map((meal) => {
+			return (
+				<div key={meal.Id}>
+					<img className="h-24 w-24 overflow-hidden"
+						src={meal.Image} alt={meal.Name} />
+					{meal.Name}
+					<a href="" className="block">Edit Meal</a>
+					<a href="" className="block">Delete Meal</a>
+				</div>
+			)
+		})
+	}
 	render() {
 		console.log(this.props.meals)
 		return (
 			<div>
-				Meallist
+				<h2>Meals</h2>
+				{this.renderMealsList()}
 			</div>
 		)
 	}
