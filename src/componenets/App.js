6ce3a4109d16571c112c002ed1cb29b39ef73b12
Kevin <kevintrn99@gmail.com>
2020-06-28 21:22:11 -0400

    setup axios

--- src/componenets/App.js
+++ src/componenets/App.js
@@ -1,11 +1,12 @@
 import '../assets/main.css'
 import React, { Component } from 'react'
 
+import MealList from './MealList'
 export default class App extends Component {
 	render() {
 		return (
 			<div>
-				App
+				<MealList />
 			</div>
 		)
 	}
